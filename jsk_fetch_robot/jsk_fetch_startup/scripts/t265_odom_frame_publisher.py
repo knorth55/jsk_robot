2ca9dead265853ec20ab5b1c09772c2b153e5416
Koki Shinjo <sktometometo@gmail.com>
2020-02-28 18:46:22 +0900

    update t265_odom_frame_publisher.py

--- jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
@@ -9,23 +9,29 @@ from geometry_msgs.msg import TransformStamped
 class OdomFramePublisher(object):
 
     def __init__(self):
+        rospy.init_node('odom_frame_publisher')
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
         self.sub = rospy.Subscriber(
                 '~input', Odometry, self._cb, queue_size=1)
         self.broadcaster = tf2_ros.StaticTransformBroadCaster()
-        self.parent_frame_id = rospy.get_param('~parent_frame_id','map')
-        self.child_frame_id = rospy.get_param('~child_frame_id','t265_pose_frame')
+        self.map_frame_id = rospy.get_param('~map_frame_id','map')
+        self.odom_frame_id = rospy.get_param('~odom_frame_id','t265_odom_frame')
+        self.pose_frame_id = rospy.get_param('~pose_frame_id','t265_pose_frame')
         self.flag = True
 
     def _cb(self, msg):
         self.sub.unregister()
         if self.flag:
             self.flag = False
-
             static_transformStamped = TransformStamped()
 
+            msg.header.frame_id = self.pose_frame_id
+            self.tfBuffer.transform( msg, self.map_frame_id )
+
             static_transformStamped.header.stamp = rospy.Time.now()
-            static_transformStamped.header.frame_id = self.parent_frame_id
-            static_transformStamped.child_frame_id = self.child_frame_id
+            static_transformStamped.header.frame_id = self.map_frame_id
+            static_transformStamped.child_frame_id = self.odom_frame_id
 
             static_transformStamped.transform.translation.x = msg.pose.pose.position.x
             static_transformStamped.transform.translation.y = msg.pose.pose.position.y
