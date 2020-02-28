a25fe4659bb3cf683dfd26dca1497ebfa8ba1c10
Koki Shinjo <sktometometo@gmail.com>
2020-02-28 19:47:46 +0900

    update fetch_t265 and script so that 2 tf tree can be bridged

--- jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
@@ -14,35 +14,38 @@ class OdomFramePublisher(object):
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
         self.sub = rospy.Subscriber(
                 '~input', Odometry, self._cb, queue_size=1)
-        self.broadcaster = tf2_ros.StaticTransformBroadCaster()
+        self.broadcaster = tf2_ros.StaticTransformBroadcaster()
         self.map_frame_id = rospy.get_param('~map_frame_id','map')
         self.odom_frame_id = rospy.get_param('~odom_frame_id','t265_odom_frame')
         self.pose_frame_id = rospy.get_param('~pose_frame_id','t265_pose_frame')
+        self.internal_pose_frame_id = rospy.get_param('~internal_pose_frame_id','internal_t265_pose_frame')
         self.flag = True
+
+        rospy.loginfo('Initialization finished')
 
     def _cb(self, msg):
         self.sub.unregister()
         if self.flag:
             self.flag = False
+
+            tf_pose2map = self.tfBuffer.lookup_transform(
+                    self.map_frame_id,
+                    self.pose_frame_id,
+                    rospy.Time(0))
+            tf_interpose2map = self.tfBuffer.lookup_transform(
+                    self.map_frame_id,
+                    self.internal_pose_frame_id,
+                    rospy.Time(0))
+            tf_
+
             static_transformStamped = TransformStamped()
-
-            msg.header.frame_id = self.pose_frame_id
-            self.tfBuffer.transform( msg, self.map_frame_id )
-
             static_transformStamped.header.stamp = rospy.Time.now()
             static_transformStamped.header.frame_id = self.map_frame_id
             static_transformStamped.child_frame_id = self.odom_frame_id
 
-            static_transformStamped.transform.translation.x = msg.pose.pose.position.x
-            static_transformStamped.transform.translation.y = msg.pose.pose.position.y
-            static_transformStamped.transform.translation.z = msg.pose.pose.position.z
+            self.broadcaster.sendTransform( static_transformStamped )
 
-            static_transformStamped.transform.rotation.x = msg.pose.pose.orientation.x
-            static_transformStamped.transform.rotation.y = msg.pose.pose.orientation.y
-            static_transformStamped.transform.rotation.z = msg.pose.pose.orientation.z
-            static_transformStamped.transform.rotation.w = msg.pose.pose.orientation.w
-
-            boardcaster.sendTransform( static_transformStamped )
+            rospy.loginfo('broadcasting tf')
 
 if __name__=='__main__':
     odompub = OdomFramePublisher()
