7d50b852a65b3e2776d6febd6b4f8027146b51a2
Koki Shinjo <sktometometo@gmail.com>
2020-03-02 18:14:36 +0900

    update t265_odom_publisher.py

--- jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/t265_odom_frame_publisher.py
@@ -23,6 +23,9 @@ class OdomFramePublisher(object):
         self.t265_odom_frame_id = rospy.get_param('~t265_odom_frame_id','t265_odom_frame')
         self.t265_pose_frame_id = rospy.get_param('~t265_pose_frame_id','t265_pose_frame')
         self.internal_pose_frame_id = rospy.get_param('~internal_pose_frame_id','internal_t265_pose_frame')
+        wait_service_name = rospy.get_param('~wait_service','/t265_to_base_link_virtual_joint/get_loggers')
+        #
+        rospy.wait_for_service(wait_service_name, timeout=30.0)
         # Subscriber & Publisher
         self.listener = tf.TransformListener()
         self.broadcaster = tf.TransformBroadcaster()
@@ -53,14 +56,19 @@ class OdomFramePublisher(object):
         if self.flag:
             self.flag = False
 
-            tf_pose2odom = self.listener.lookupTransform(
-                    self.t265_pose_frame_id,
-                    self.t265_odom_frame_id,
-                    rospy.Time(0))
-            tf_map2pose = self.listener.lookupTransform(
-                    self.map_frame_id,
-                    self.internal_pose_frame_id,
-                    rospy.Time(0))
+            while True:
+                try:
+                    tf_pose2odom = self.listener.lookupTransform(
+                            self.t265_pose_frame_id,
+                            self.t265_odom_frame_id,
+                            rospy.Time(0))
+                    tf_map2pose = self.listener.lookupTransform(
+                            self.map_frame_id,
+                            self.internal_pose_frame_id,
+                            rospy.Time(0))
+                    break
+                except:
+                    rospy.logwarn('lookup transform has failed.')
 
             frame_pose2odom = pm.fromTf( tf_pose2odom )
             frame_map2pose = pm.fromTf( tf_map2pose )
