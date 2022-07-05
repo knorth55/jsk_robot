4a9f3864f4e7d2d9b30e642db4a05c16aa17ee3b
Koki Shinjo <sktometometo@gmail.com>
2022-07-06 08:42:32 +0900

    Revert "[trigger_behavior_recovery] update"
    This reverts commit eb08eb3434d07a0722e169f4be737070aee56abc.

--- jsk_robot_common/trigger_behavior_recovery/node_scripts/speak_and_wait_behavior.py
+++ jsk_robot_common/trigger_behavior_recovery/node_scripts/speak_and_wait_behavior.py
@@ -3,8 +3,7 @@
 import rospy
 import actionlib
 from sound_play.libsoundplay import SoundClient
-from trigger_behavior_msgs.msg import TriggerBehaviorAction
-from trigger_behavior_msgs.msg import TriggerBehaviorResult
+from trigger_behavior_recovery.msg import
 
 
 class SpeakAndWaitBehavior(object):
@@ -14,27 +13,12 @@ class SpeakAndWaitBehavior(object):
         self._speak_text = rospy.get_param('~speak_text', 'Hello World!')
         self._duraiton_wait = rospy.get_param('~duration_wait', 5.0)
         self._sound_action_name = rospy.get_param('~sound_action', 'sound_play')
-        self._sound_client = SoundClient(
-            blocking=True,
-            sound_action=self._sound_action_name)
-        self._action_server = actionlib.SimpleActionServer(
-            '~behavior',
-            TriggerBehaviorAction,
-            self.handler,
-            False)
+
+        self._sound_client = SoundClient(blocking=True, sound_action = self._sound_action_name)
+
+        self._action_server = 
 
     def handler(self, goal):
 
         self._sound_client.say(self._speak_text)
         rospy.sleep(self._duration_wait)
-
-        result = TriggerBehaviorResult()
-        result.success = True
-        self._action_server.set_succeeded(result)
-
-
-if __name__ == '__main__':
-
-    rospy.init_node('speak_and_wait_behavior')
-    node = SpeakAndWaitBehavior()
-    rospy.spin()
